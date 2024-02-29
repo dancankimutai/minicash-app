ebf612fc47c583c60874164e28dc34cc54015676
Duncan Kimutai <59916500+dancankimutai@users.noreply.github.com>
2024-02-29 10:56:51 +0300

    update transfer function

--- packages/react-app/utils/withdraw.ts
+++ packages/react-app/utils/withdraw.ts
@@ -1,17 +1,22 @@
-import { ethers} from "ethers";
+
+import { BrowserProvider, Contract, parseEther } from "ethers";
 const CUSD_ADDRESS = "0x765de816845861e75a25fca122bb6898b8b1282a";///cusd contract address
+const The_AMOUNT = parseEther(amount); // amount in CUSD
+
 
 export const transferCUSD = async (address: string, userAddress: string ,amount: string) => {
      if (window.ethereum) {
 //Get connected accounts, if not connected request connection. 
-        const provider = new ethers.providers.Web3Provider(window.ethereum); 
+        const provider = new BrowserProvider(window.ethereum); 
         const signer = await provider.getSigner(userAddress);
         
 // The current selected account out of the connected accounts.
         let abi =["function transfer(address to, uint256 value)"]; 
-        const CUSDContract = new ethers.Contract (CUSD_ADDRESS, abi, signer);
+        const CUSDContract = new Contract (CUSD_ADDRESS, abi, signer);
 
-    // Transfer the  amount to the external address.
-    let txn = await CUSDContract.transfer(address, (ethers.utils.parseEther(amount)));
-       
+
+    // Transfer the amount to your address.
+        const Txn = await CUSDContract.transfer(address, The_AMOUNT);
+        let Receipt = await Txn.wait();
+
 }};
